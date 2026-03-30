63ccf914ea86b72a438cf92d365c4ecc2f7501f0
aayush-lang <aayush@otakuhaus.com>
2026-03-30 22:48:12 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://hsudagdnygfiggpahsit.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_oY0LZlb0acdZ7S_Xqiv38A_93cIOdVO';
+const RESEND_API_KEY = 'YOUR_RESEND_API_KEY';
+const FROM_EMAIL = 'crm@riddlermedia.in';
+export { SUPABASE_URL, SUPABASE_ANON_KEY, RESEND_API_KEY, FROM_EMAIL };
